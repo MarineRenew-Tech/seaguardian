bc1e757fed3b4932fc3d8c47e47697da7ac0eead
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-05-30 18:36:34 -0300

    adicionando mais links no header, e estilizando a função de aparecer e desaparecer dos textos iniciais

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
-let p2 = document.querySelector(".texto2");
-let p1 = document.querySelector(".texto1");
-window.addEventListener('scroll',function(){
-    if(this.window.scrollY <= this.window.innerHeight * 0.3){
-       p1.style.transform = "translateX(0)";
-       p2.style.transform= "translateX(110%)";
-       p1.style.transition = "1s";
-    }else{
-        p1.style.transform = "translateX(-100%)";
-        p2.style.transform = "translateX(0)";
-        p2.style.transition= "1s";
-    }
-})
+document.addEventListener('DOMContentLoaded', function(){
+    let p2 = document.querySelector(".texto2");
+    let p1 = document.querySelector(".texto1");
+
+  
+    p1.style.transform = "translateX(-100%)";
+    p1.style.opacity = "0";
+
+    setTimeout(() => {
+        p1.style.transform = "translateX(0)";
+        p1.style.transition = "transform 1s, opacity 1s"
+        p1.style.opacity = "1";
+    },500);
+
+    window.addEventListener('scroll',function(){
+        if(this.window.scrollY <= this.window.innerHeight * 0.3){
+           p1.style.transform = "translateX(0)";
+           p1.style.opacity = "1";
+           p2.style.opacity = "0";
+           p2.style.transform= "translateX(110%)";
+           p1.style.transition = "1s";
+        }else{
+            p1.style.transform = "translateX(-100%)";
+            p2.style.transform = "translateX(0)";
+            p1.style.opacity = "0";
+            p2.style.opacity = "1";
+            p2.style.transition= "1s";
+        }
+    });
+});
+
+
